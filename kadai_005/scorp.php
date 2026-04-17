daf3e5331c4c33e318b6bcb24e83a7d17d4e5139
ku08070807-create <ku08070807@gmail.com>
2026-04-17 12:56:56 +0900

    Add files via upload

---
+++ kadai_005/scorp.php
@@ -0,0 +1,19 @@
+<?php
+$score = 88;
+echo $score;
+$score = 60;
+echo $score;
+$score = 55;
+echo $score;
+$score = 40;
+echo $score;
+$score = 100;
+echo $score;
+$score = 80;
+echo $score;
+$score = 95;
+echo $score;
+$score = 30;
+echo $score;
+$score = 60;
+echo $score;
