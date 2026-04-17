468436c7f79583e77c3a9de8842a51e7c0a71895
ku08070807-create <ku08070807@gmail.com>
2026-04-17 22:26:45 +0900

    Add files via upload

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -18,12 +18,14 @@
         $array = array(23, 18, 15, 10, 4);
         $sorted_array = function_2way_sort($array);
         print_r($sorted_array);
-        ?>
-     function_2way_sort($array) {
+
+   function sort_2way($array) {
             $sorted_array = $array;
             sort($sorted_array);
             return $sorted_array;
         }
+        ?>
+      
 
     </p>
 
