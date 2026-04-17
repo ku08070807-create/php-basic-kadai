d9a52c649515a334c05dfb25a7955d420797af85
ku08070807-create <ku08070807@gmail.com>
2026-04-17 22:39:56 +0900

    Add files via upload

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -9,6 +9,16 @@
 <body>
     <p>
         <?php
+        function sort_2way($array,$order) {
+          if ($order === TRUE) {
+                echo "昇順にソートします。<br>";
+                sort($array);
+            }
+             else {
+                echo "降順にソートします。<br>";
+                rsort($array);
+            }
+
         // ここにコードを書いていく
         $array = array(4, 10, 15, 18, 23);
         asort($array);
@@ -18,26 +28,11 @@
         echo "<br>";
 
         $array = array(23, 18, 15, 10, 4);
-        $sorted_array = sort_2way($array, "asc");
+        $sorted_array = sort_2way($array, TRUE);
         foreach($sorted_array as $value) {
             echo $value . " ";
         }
-
-   function sort_2way($array,$order) {
-            $sorted_array = $array;
-            sort($sorted_array);
-            return $sorted_array;
-        }
-
-
-if ($order == "asc") {
-            sort($sorted_array);
-        } else if ($order == "desc") {
-            rsort($sorted_array);
-        }
         ?>
-      
-
     </p>
 
 
